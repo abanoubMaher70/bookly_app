1c81d45d0c0d2a18fc952dc6ebe168ea79ca5d5e
Abanoub Maher <abanoub.maher70@gmail.com>
2025-03-12 00:02:19 +0200

    create similar books section and there list view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/books_details_section.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -17,6 +18,7 @@ class BookDetailsViewBody extends StatelessWidget {
             SizedBox(height: 10),
             BookDetailsSection(),
             SizedBox(height: 28),
+            SimilarBooksSection(),
           ],
         ),
       ),
---
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/features/home/presentation/views/widgets/book_cover_item.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.22,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return const BookCoverItem(borderRadius: 8);
+        },
+      ),
+    );
+  }
+}
